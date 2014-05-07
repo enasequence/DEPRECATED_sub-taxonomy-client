3641e4b7c7de0f6e77833c29517a4e1727bd03fa
Daniel Vaughan <flat@danielvaughan.com>
2014-05-07 11:26:25 +0100

    Initial Commit

---
+++ src/main/java/uk/ac/ebi/ena/taxonomy/client/TaxonomyClientImpl.java
@@ -0,0 +1,5 @@
+package uk.ac.ebi.ena.taxonomy.client;
+
+public class TaxonomyClientImpl implements TaxonomyClient {
+
+}
---
+++ src/test/java/uk/ac/ebi/ena/taxonomy/client/TaxonomyClientIntegTest.java
@@ -0,0 +1,14 @@
+package uk.ac.ebi.ena.taxonomy.client;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TaxonomyClientIntegTest {
+
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}
