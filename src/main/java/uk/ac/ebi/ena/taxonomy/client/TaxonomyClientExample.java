567b71d222856924b09099dcc764360af0b89f44
Daniel Vaughan <dvaughan@ebi.ac.uk>
2015-10-09 12:08:12 +0100

    Created version 2.0.0 for WI-105

--- src/main/java/uk/ac/ebi/ena/taxonomy/client/TaxonomyClientExample.java
+++ src/main/java/uk/ac/ebi/ena/taxonomy/client/TaxonomyClientExample.java
@@ -7,51 +7,50 @@ import java.util.List;
 
 public class TaxonomyClientExample {
 
-   TaxonomyClient taxonomyClient;
+    TaxonomyClient taxonomyClient;
 
-  public TaxonomyClientExample(String serviceUrl) {
-    taxonomyClient = new TaxonomyClientCurrentImpl(serviceUrl);
-  }
-
-  public static void main(final String[] args) {
-    if (args.length != 2) {
-      System.out.println("Parameters: serviceUrl query");
-    } else {
-      final String serviceUrl = args[0];
-      final String partialName = args[1];
-      TaxonomyClientExample taxonomyClientExample = new TaxonomyClientExample(serviceUrl);
-      taxonomyClientExample.query(partialName);
+    public TaxonomyClientExample(String serviceUrl) {
+        taxonomyClient = new TaxonomyClientCurrentImpl(serviceUrl);
     }
-  }
 
-  private void query(String partialName) {
-
-    try {
-      final List<Taxon> taxonSuggestionList = taxonomyClient.suggestTaxa(partialName);
-      outputList(taxonSuggestionList);
-    } catch (final TaxonomyException ex) {
-      System.out.println("Query failed with message: " + ex.getMessage());
+    public static void main(final String[] args) {
+        if (args.length != 2) {
+            System.out.println("Parameters: serviceUrl query");
+        } else {
+            final String serviceUrl = args[0];
+            final String partialName = args[1];
+            TaxonomyClientExample taxonomyClientExample = new TaxonomyClientExample(serviceUrl);
+            taxonomyClientExample.query(partialName);
+        }
     }
-  }
 
-  private void outputList(List<Taxon> taxonSuggestionList) {
-    StringBuilder sb = new StringBuilder();
-    sb.append("Tax ID");
-    sb.append('\t');
-    sb.append("Common Name");
-    sb.append('\t');
-    sb.append("Scientific Name");
-    sb.append('\n');
-    for (Taxon taxon : taxonSuggestionList)
-    {
-      sb.append(taxon.getTaxId());
-      sb.append('\t');
-      sb.append(taxon.getCommonName());
-      sb.append('\t');
-      sb.append(taxon.getScientificName());
-      sb.append('\n');
+    private void query(String partialName) {
+
+        try {
+            final List<Taxon> taxonSuggestionList = taxonomyClient.suggestTaxa(partialName);
+            outputList(taxonSuggestionList);
+        } catch (final TaxonomyException ex) {
+            System.out.println("Query failed with message: " + ex.getMessage());
+        }
     }
-    System.out.println(sb.toString());
-  }
+
+    private void outputList(List<Taxon> taxonSuggestionList) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Tax ID");
+        sb.append('\t');
+        sb.append("Common Name");
+        sb.append('\t');
+        sb.append("Scientific Name");
+        sb.append('\n');
+        for (Taxon taxon : taxonSuggestionList) {
+            sb.append(taxon.getTaxId());
+            sb.append('\t');
+            sb.append(taxon.getCommonName());
+            sb.append('\t');
+            sb.append(taxon.getScientificName());
+            sb.append('\n');
+        }
+        System.out.println(sb.toString());
+    }
 
 }
